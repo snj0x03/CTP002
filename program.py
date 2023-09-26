6aa6bd5adfb1b97aecb3db8fd3bc0a450e535619
snj0x03 <pyy.otc@gmail.com>
2023-09-26 19:47:22 +0900

    deleted:    SecondsConvertProgram.py 	deleted:    TemperatureProgram.py 	deleted:    a.exe 	deleted:    cprime.cpp 	deleted:    homeowkr.py 	deleted:    largeprime.cpp 	deleted:    p1.py 	deleted:    program.c 	deleted:    program.exe 	deleted:    program.py 	deleted:    program2.c 	deleted:    test.cpp 	modified:   test.py

--- program.py
+++
@@ -1,2 +0,0 @@
-print("My name is Saw Nay Soe and student id is 202312244")
-print("Hello")
--- test.py
+++ test.py
@@ -1,8 +1,44 @@
-from math import ceil
+try:
+    Hours = int(input("Enter hours : "))
+    Rate = int(input("Enter rate : "))
+    if Hours > 40:
+        Salary = (40 * Rate) + (1.5 * (Hours - 40) * Rate)
+    else:   
+        Salary = Hours * Rate
+    print("Pay :", Salary)
+except:
+    print("Error, please enter numeric input")
 
-sumDiv_3 = sum([3*i for i in range(ceil(1000/3))])
-sumDiv_5 = sum([5*i for i in range(ceil(1000/5))])
-sumDiv_15 = sum([15*i for i in range(ceil(1000/15))])
+try:
+    Score = int(input("Enter Score : "))
+    if Score >= 90 and Score <= 100:
+        print("Grade is A")
+    elif Score >= 80:
+        print("Grade is B")
+    elif Score >= 70:
+        print("Grade is C")
+    elif Score >= 60:
+        print("Grade is D")
+    elif Score >= 0 and Score < 60:
+        print("Grade is F")
+except:
+    print("Error, please enter numeric input")
+else:
+    print("Error, please enter numeric input")
 
-ans = sumDiv_3 + sumDiv_5 - sumDiv_15
-print(ans)
+number = 0
+sum = 0
+count = 0
+while True:
+    try:
+        number = input("Enter a number : ")
+        if number == "done":
+            break
+        number = int(number)
+        sum += number
+        count += 1
+    except:
+        print("Error")
+print("Sum of input numbers :", sum)
+print("number of input : ", count)
+print("Average of input number : ", sum/count)
