b4152e7d2e35826fef83c640211bc4151d9e64b3
Saw Nay Soe <81210802+snj0x03@users.noreply.github.com>
2023-09-26 19:45:38 +0900

    Delete SecondsConvertProgram.py

--- SecondsConvertProgram.py
+++
@@ -1,5 +0,0 @@
-S = int(input("Enter seconds : "))
-Hours = S // 3600
-Minutes = (S%3600) // 60
-Seconds = S % 60
-print(str(S), "seconds is", str(Hours), "hours,", str(Minutes), "minutes,", str(Seconds), "seconds")
