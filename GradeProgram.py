f431b47697404d2c045143113ba8d6f4b6f2e8a8
snj0x03 <pyy.otc@gmail.com>
2023-10-03 13:40:15 +0900

    modified:   GradeProgram.py

--- GradeProgram.py
+++ GradeProgram.py
@@ -1,19 +1,22 @@
 def Grade(Score):
     if Score >= 90 and Score <= 100:
-        return "Grade is A"
+        return "A"
     elif Score >= 80 and Score < 90:
-        return "Grade is B"
+        return "B"
     elif Score >= 70 and Score < 90:
-        return "Grade is C"
+        return "C"
     elif Score >= 60 and Score < 70:
-        return "Grade is D"
+        return "D"
     elif Score >= 0 and Score < 60:
-        return "Grade is F"
+        return "F"
     else:
-        return "Error, please enter numeric input between 0 and 100"
+        return "error"
 
 try:
     Score = int(input("Enter Score : "))
-    print(Grade(Score))
+    if Grade(Score) != "error":
+        print("Grade is", Grade(Score))
+    else:
+        print("Error, please enter numeric input between 0 and 100")
 except:
     print("Error, please enter numeric input between 0 and 100")
