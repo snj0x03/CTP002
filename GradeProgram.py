5251ae9c2484e4e04bb18a5e478b2abb92a52c5b
snj0x03 <pyy.otc@gmail.com>
2023-10-03 13:29:09 +0900

    new file:   DivisionProgram.py 	modified:   GradeProgram.py 	deleted:    RepeatedNumberProgram.py 	modified:   SalaryProgram.py

--- GradeProgram.py
+++ GradeProgram.py
@@ -1,16 +1,19 @@
+def Grade(Score):
+    if Score >= 90 and Score <= 100:
+        return "Grade is A"
+    elif Score >= 80 and Score < 90:
+        return "Grade is B"
+    elif Score >= 70 and Score < 90:
+        return "Grade is C"
+    elif Score >= 60 and Score < 70:
+        return "Grade is D"
+    elif Score >= 0 and Score < 60:
+        return "Grade is F"
+    else:
+        return "Error, please enter numeric input between 0 and 100"
+
 try:
     Score = int(input("Enter Score : "))
-    if Score >= 90 and Score <= 100:
-        print("Grade is A")
-    elif Score >= 80 and Score < 90:
-        print("Grade is B")
-    elif Score >= 70 and Score < 90:
-        print("Grade is C")
-    elif Score >= 60 and Score < 70:
-        print("Grade is D")
-    elif Score >= 0 and Score < 60:
-        print("Grade is F")
-    else:
-        print("Error, please enter numeric input between 0 and 100")
+    print(Grade(Score))
 except:
     print("Error, please enter numeric input between 0 and 100")
--- SalaryProgram.py
+++ SalaryProgram.py
@@ -1,10 +1,13 @@
+def computepay(hours, rate):
+    if hours > 40:
+        Pay = (40 * rate) + (1.5 * (hours - 40) * rate)
+    else:   
+        Pay = hours * rate
+    return Pay 
+
 try:
     Hours = int(input("Enter Hours : "))
     Rate = int(input("Enter Rate : "))
-    if Hours > 40:
-        Salary = (40 * Rate) + (1.5 * (Hours - 40) * Rate)
-    else:   
-        Salary = Hours * Rate
-    print("Pay :", Salary)
+    print("Pay :", computepay(Hours, Rate))
 except:
     print("Error, please enter numeric input")
